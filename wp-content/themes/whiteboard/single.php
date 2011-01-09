1b48d2ff559e46f40eb83f7eed8b6718e780768e
Brian Andrich <brianandrich@Brian-Andrichs-MacBook.local>
2011-01-09 16:33:09 -0500

    comments1

--- wp-content/themes/whiteboard/single.php
+++ wp-content/themes/whiteboard/single.php
@@ -6,9 +6,7 @@
 		<div id="post-<?php the_ID(); ?>" <?php post_class('post'); ?>>
 			<article>
 				<h1><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-				<?php edit_post_link('<small>Edit this entry</small>','',''); ?>
-				<?php echo '<div class="featured-thumbnail">'; the_post_thumbnail(); echo '</div>'; ?> <!-- loades the post's featured thumbnail, requires Wordpress 3.0+ -->
-
+			
 				<div id="post-content">
 					<?php the_content(); ?>
 					<div class="pagination">
