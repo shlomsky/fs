17a7217d7a8903735c5601f2c8ac8fb0e661c462
Brian Andrich <brianandrich@Brian-Andrichs-MacBook.local>
2011-01-09 16:13:18 -0500

    agh

--- wp-content/themes/whiteboard/comments.php
+++ wp-content/themes/whiteboard/comments.php
@@ -59,9 +59,7 @@
 		            <?php if($user_ID) : ?>
 		                <p>Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout" title="Log out of this account">Log out &raquo;</a></p>
 		            <?php else : ?>
-		            	<p>
-		            		Allowed tags: <?php echo allowed_tags(); ?>
-		            	</p>
+		            
 		                <p>
 							<label for="author"><small>Name <?php if($req) echo "(required)"; ?></small></label>
 							<input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="22" tabindex="1" />
@@ -70,10 +68,7 @@
 		                	<label for="email"><small>Mail (will not be published) <?php if($req) echo "(required)"; ?></small></label>
 		                	<input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="22" tabindex="2" />
 		                </p>
-		                <p>
-		                	<label for="url"><small>Website</small></label>
-		                	<input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="22" tabindex="3" />
-		                </p>
+		              
 		            <?php endif; ?>
 		            <p>
 		            	<label for="comment"><small>Comment</small></label>
@@ -85,7 +80,7 @@
 		            </p>
 		            <?php do_action('comment_form', $post->ID); ?>
 		        </form>
-				<p><small>By submitting a comment you grant <?php bloginfo('name'); ?> a perpetual license to reproduce your words and name/web site in attribution. Inappropriate and irrelevant comments will be removed at an admin’s discretion. Your email is used for verification purposes only, it will never be shared.</small></p>
+				
 		    <?php endif; ?>
 		<?php else : ?>
 		    <p>The comments are closed.</p>
