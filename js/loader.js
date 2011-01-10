7c74497c7ac9d8e1f442997cf66759df8876a421
Brian Andrich <brianandrich@Brian-Andrichs-MacBook.local>
2011-01-10 09:30:42 -0500

    new img7

--- js/loader.js
+++ js/loader.js
@@ -4,7 +4,7 @@ randomNumber = Math.round(Math.random()*(bgImageTotal-1))+1;
 
 // Initialize Backgound Stretcher
 $(document).bgStretcher({
-images: ['./images/bg/'+randomNumber+'.jpg'],
+images: ['/images/bg/'+randomNumber+'.jpg'],
 imageWidth: 1024,
 imageHeight: 768,
 slideShow: false
