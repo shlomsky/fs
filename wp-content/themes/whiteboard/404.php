613b1d52c28d27f36bf8f4fa64f5c56efcde5ffb
Brian Andrich <brianandrich@Brian-Andrichs-MacBook.local>
2011-01-10 09:37:59 -0500

    new img9

--- wp-content/themes/whiteboard/404.php
+++ wp-content/themes/whiteboard/404.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 <?php get_sidebar(); ?>
 <div id="content">
+		<div class="content-item">
 	<div id="error404" class="post">
 		<h1>Error 404 Not Found</h1>
 		<div class="postContent">
@@ -9,6 +10,7 @@
 			<?php get_search_form(); ?> <!-- outputs the default Wordpress search form-->
 		</div><!--.post-content-->
 	</div><!--#error404 .post-->
+	</div>
 </div><!--#content-->
 
 <?php get_footer(); ?>
