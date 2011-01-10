648b2570b2cec30d661eb2779439f81bf49e8d9c
Brian Andrich <brianandrich@Brian-Andrichs-MacBook.local>
2011-01-10 09:37:14 -0500

    new img8

--- wp-content/themes/whiteboard/404.php
+++ wp-content/themes/whiteboard/404.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
+<?php get_sidebar(); ?>
 <div id="content">
 	<div id="error404" class="post">
 		<h1>Error 404 Not Found</h1>
@@ -9,5 +10,5 @@
 		</div><!--.post-content-->
 	</div><!--#error404 .post-->
 </div><!--#content-->
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
