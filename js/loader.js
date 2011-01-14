6f0fda6a1c92f2eeb2cb801a0a58b8dc711b26da
Brian Andrich <brianandrich@Brian-Andrichs-MacBook.local>
2011-01-13 20:44:05 -0500

    new img13

--- js/loader.js
+++ js/loader.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-bgImageTotal=9;
+bgImageTotal=21;
 randomNumber = Math.round(Math.random()*(bgImageTotal-1))+1;
 
 // Initialize Backgound Stretcher
 $(document).bgStretcher({
 images: ['/images/bg/'+randomNumber+'.jpg'],
 imageWidth: 1024,
-imageHeight: 768,
+imageHeight: 683,
 slideShow: false
 });
 
